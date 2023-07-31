6bee1f79912a5f02039c1bb34e27a387a807c4f3
kiruxin <kiruxin16@gmail.com>
2023-07-31 06:30:25 +0300

    Schedule MS

---
+++ schedule/src/test/java/threeoceans/fitness/ru/accounts/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package threeoceans.fitness.ru.accounts.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
